fbe651a1d6cfffa7b509087ebb7348e2095ce62d
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-11-14 17:49:21 -0600

    added pop, index,head and tail funcions to linked list class

--- linked_list.rb
+++ linked_list.rb
@@ -7,7 +7,7 @@
 #size returns the total number of nodes in the list
 
 #head returns the first node in the list
-# --> WORKING FROM HERE - - - - - - - - - - - - - - - - - - -
+
 #tail returns the last node in the list
 
 #at(index) returns the node at the given index
@@ -41,22 +41,27 @@ class LinkedList
 	end
 
 	def head
+	  data_set[0]
 	end
 
 	def tail
+	  data_set[data_set.length-1]
 	end
-
+# --> WORKING FROM HERE - - - - - - - - - - - - - - - - - - -
 	def at(index)
+	  data_set[index]
 	end
 
 	def pop
+	  data_set.pop
 	end
 
 	def contains?(node)
+	  data_set.include?(node)
 	end
 
 	def find(data)
-		#return index of node
+	  a.index(data)
 	end
 	
 	def to_s
@@ -72,7 +77,23 @@ end
 
 test_list = LinkedList.new
 test_list.append('AA')
+test_list.append('XX')
+test_list.append('remove me')
 test_list.prepend('BB')
-test_list.size
+
+# puts ' - - - -'
+# p test_list.size
+# puts ' - - - -'
+# p test_list.head
+# puts ' - - - -'
+# p test_list.tail
+p test_list.data_set
+puts ' - - - -'
+p test_list.at(1)
+puts ' - - - -'
+test_list.pop
+p test_list.data_set
+# p test_list.data_set
+
 
 
