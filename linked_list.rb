eda6e754cfe16664dab4fbf8c31bee260b467bd0
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-11-14 17:36:12 -0600

    creatted append, prepend and size functions in Linked list class

--- linked_list.rb
+++ linked_list.rb
@@ -5,7 +5,7 @@
 #prepend adds a new node to the start of the list
 
 #size returns the total number of nodes in the list
-
+# --> WORKING FROM HERE - - - - - - - - - - - - - - - - - - -
 #head returns the first node in the list
 
 #tail returns the last node in the list
@@ -28,16 +28,29 @@ class LinkedList
 		@data_set = []
 	end
 	
-	def append(node_x)
-      data_set.push(node_x)
+	def append(new_node)
+      data_set.push(new_node)
 	end	
 
-	def prepend(node_x)
-	  data_set.unshift(node_x)	
+	def prepend(new_node)
+	  data_set.unshift(new_node)	
 	end	
+
+	def size
+	  data_set.length	
+	end
 
 end
 
 class Node
 
 end	
+
+#runner - - - - -
+
+test_list = LinkedList.new
+test_list.append('AA')
+test_list.prepend('BB')
+puts test_list.size
+
+
