9d1515d78259816ba9e1875a85a1f59701a8b881
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-11-15 16:47:11 -0600

    third try

--- linked_list.rb
+++ linked_list.rb
@@ -3,7 +3,38 @@
 # ['barbra','Lilly','Adam','Cheryl','Karl']
 
 class LinkedList
-	
+    attr_accessor :list, 
+
+
+	def initialize
+		@list = []
+	end
+
+	def append(value)
+		new_node = Node.new(value, nil)
+		if list.length == 0
+			list << new_node
+		else 
+			list << new_node
+		    list[list.length-2].next_node = new_node.value	
+		end
+	end
+
+    def prepend(value)
+        new_node = Node.new(value,nil)
+    	if list.empty?
+    	  list.unshift(new_node)
+    	else 
+    	  new_node.next_node = list[0].value
+    	  list.unshift(new_node)
+    	end	
+    end
+
+    def size
+    	list.length
+    end
+
+
 end
 
 class Node
@@ -16,10 +47,19 @@ class Node
 end	
 
 #runner - - - - -
+p 'lilililil'
+test_link = LinkedList.new
 
-testing_linked_list = LinkedList.new
+test_link.append('abcdefg')
+# test_link.append('parabola')
+# test_link.append('this is the END!!!')
+
+# test_link.prepend('IN THE BEGINNING there was coffee')
+# p test_link.list.size
+#test_link
+# [ NODE(head) ] -> [ NODE ] -> [ NODE(tail) ] -> nil
 
 
-
-
-
+#<LinkedList:
+  # @head= <Node@value="head", @next_node=nil>, 
+   #@tail=#<Node:@value="xxx", @next_node=nil>>
